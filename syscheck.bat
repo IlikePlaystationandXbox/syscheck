5dd0cb18c6c0aa95d2e9237450a5cab4ec23cf22
IlikePlaystationandXbox <128911284+IlikePlaystationandXbox@users.noreply.github.com>
2023-09-15 23:18:27 +0100

    Add files via upload

---
+++ syscheck.bat
@@ -0,0 +1,46 @@
+@echo off
+cls
+pause
+cls
+echo ======================
+echo       PC VERSION
+echo ======================
+ver
+pause
+cls
+echo =========================
+echo       ADVANCED INFO
+echo =========================
+systeminfo
+echo Scroll up to find advanced info list.
+pause
+cls
+echo ============================
+echo       MINECRAFT FOLDER
+echo ============================
+dir C:\Users\you\Appdata\Roaming\.minecraft\
+pause
+cls
+echo ==================================
+echo       APPDATA - ROAMING INFO
+echo ==================================
+dir C:\Users\you\Appdata\Roaming\
+pause
+cls
+echo ================================
+echo       APPDATA - LOCAL INFO
+echo ================================
+dir C:\Users\you\Appdata\Local\
+pause
+cls
+echo ======================================
+echo       APPDATA - LOCAL "LOW" INFO
+echo ======================================
+dir C:\Users\you\Appdata\LocalLow\
+pause
+cls
+echo =================================
+echo       LOCAL DISK "C:\" INFO
+echo =================================
+dir C:\
+pause
